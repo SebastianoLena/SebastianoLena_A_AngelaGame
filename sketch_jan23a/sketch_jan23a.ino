aef4c07db02ba3d65a3b18c814af887d29c0cdd9
Sebastiano Lena <sebastiano.lena@barsanti.edu.it>
2019-01-23 21:23:23 +0100

    Errori precedenti corretti e varie prove riguardanti il monitor seriale

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -1,45 +1,73 @@
 int totale; //Indica il risultato a cui bisogna arrivare per vincere
-int mossaGiocatore; //Indica il numero che ha inserito il giocatore durante la sua mossa
-char numeroCorrente; //Indica il numero corrente a cui si è arrivati
+char mossaGiocatore; //Indica il numero che ha inserito il giocatore durante la sua mossa
+int numeroCorrente; //Indica il numero corrente a cui si è arrivati
 int turno; // turno = 0 (giocatore 1) | turno = 1 (giocatore 2)
 char istruzioni; //Le istruzioni che verranno visualizzate nel monitor seriale
+bool finito; //Variabile booleana che verrà utilizzata nei cicli
+int cont; //Contatore che verrà utilizzato nei cicli
 
-void setup() 
+void setup()
 {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
   totale = 0;
-  mossaGiocatore = 0;
+  numeroCorrente = 0;
   turno = 0;
+  finito = false;
+  cont = 0;
 }
 
-void loop() 
-{ 
+void loop()
+{
   // put your main code here, to run repeatedly:
+  FaiMossa();
+  delay(1000);
+}
 
-  void PassaTurno()
+void DaiIStruzioni()
+{
+  Serial.print(istruzioni);
+}
+
+void PassaTurno()
+{
+  if (turno == 0)
   {
-    if (turno == 0)
+    turno = 1;
+    istruzioni = "Tocca al Giocatore 2";
+  }
+  else
+  {
+    turno = 0;
+    istruzioni = "Tocca al Giocatore 1";
+  }
+}
+void AggiungiMossaAlRisultato() //Aggiunge la mossa appena fatta al risultato corrente
+{
+  numeroCorrente = numeroCorrente + mossaGiocatore;
+  Serial.println(numeroCorrente);
+}
+
+void FaiMossa() //Permette al giocatore di fare la sua mossa
+{
+  while(!finito)
+  {
+    if(mossaGiocatore == 0)
     {
-      turno = 1;
-      istruzioni = "Tocca al Giocatore 2";
+    mossaGiocatore = Serial.read();
     }
     else
     {
-      turno = 0;
-      istruzioni = "Tocca al Giocatore 1";
+      finito = true;
     }
-    Serial.println(istruzioni);
   }
-  void AggiungiMossaAlRisultato() //Aggiunge la mossa appena fatta al risultato corrente
-  {
-    numeroCorrente = numeroCorrente + mossaGiocatore;
-    Serial.println(numeroCorrente);
-  }
+  Serial.print(mossaGiocatore);
+}
 
-  void FaiMossa() //Permette al giocatore di fare la sua mossa
-  {
-    mossaGiocatore = Serial.read();
-  }
+void TerminaPartita()
+{
+  totale = 0;
+  numeroCorrente = 0;
+  turno = 0;
 }
