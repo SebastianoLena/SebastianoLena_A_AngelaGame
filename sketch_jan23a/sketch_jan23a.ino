67b83691e8a8ab8512c78382e1726c782c09b825
Sebastiano Lena <sebastiano.lena@barsanti.edu.it>
2019-01-24 09:54:02 +0100

    Revisione del metodo FaiMossa

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -2,7 +2,7 @@ int totale; //Indica il risultato a cui bisogna arrivare per vincere
 char mossaGiocatore; //Indica il numero che ha inserito il giocatore durante la sua mossa
 int numeroCorrente; //Indica il numero corrente a cui si è arrivati
 int turno; // turno = 0 (giocatore 1) | turno = 1 (giocatore 2)
-char istruzioni; //Le istruzioni che verranno visualizzate nel monitor seriale
+//char istruzioni; //Le istruzioni che verranno visualizzate nel monitor seriale
 bool finito; //Variabile booleana che verrà utilizzata nei cicli
 int cont; //Contatore che verrà utilizzato nei cicli
 
@@ -16,6 +16,7 @@ void setup()
   turno = 0;
   finito = false;
   cont = 0;
+  mossaGiocatore = '0';
 }
 
 void loop()
@@ -25,22 +26,22 @@ void loop()
   delay(1000);
 }
 
-void DaiIStruzioni()
+/*void DaiIStruzioni()
 {
   Serial.print(istruzioni);
-}
+}*/
 
 void PassaTurno()
 {
   if (turno == 0)
   {
     turno = 1;
-    istruzioni = "Tocca al Giocatore 2";
+    //istruzioni = "Tocca al Giocatore 2";
   }
   else
   {
     turno = 0;
-    istruzioni = "Tocca al Giocatore 1";
+    //istruzioni = "Tocca al Giocatore 1";
   }
 }
 void AggiungiMossaAlRisultato() //Aggiunge la mossa appena fatta al risultato corrente
@@ -53,16 +54,16 @@ void FaiMossa() //Permette al giocatore di fare la sua mossa
 {
   while(!finito)
   {
-    if(mossaGiocatore == 0)
-    {
     mossaGiocatore = Serial.read();
-    }
-    else
+    if (mossaGiocatore > 0 && mossaGiocatore < 7) 
     {
       finito = true;
     }
   }
-  Serial.print(mossaGiocatore);
+  finito = false;
+  Serial.println(mossaGiocatore);
+  mossaGiocatore = '0';
+  
 }
 
 void TerminaPartita()
