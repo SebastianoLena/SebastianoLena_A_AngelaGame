c95abca8ab9c3f8db73754fd1bf97595d4b902ca
Sebastiano Lena <sebastiano.lena@barsanti.edu.it>
2019-01-25 09:09:32 +0100

    Perfezionamento di tutti i metodi del programma

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -1,8 +1,8 @@
-int totale; //Indica il risultato a cui bisogna arrivare per vincere
+int risDaRaggiungere; //Indica il risultato a cui bisogna arrivare per vincere
 char mossaGiocatore; //Indica il numero che ha inserito il giocatore durante la sua mossa
+int mossaAppoggio; //Questa sarà la variabile d'appoggio per le mosse del giocatore
 int numeroCorrente; //Indica il numero corrente a cui si è arrivati
 int turno; // turno = 0 (giocatore 1) | turno = 1 (giocatore 2)
-//char istruzioni; //Le istruzioni che verranno visualizzate nel monitor seriale
 bool finito; //Variabile booleana che verrà utilizzata nei cicli
 int cont; //Contatore che verrà utilizzato nei cicli
 
@@ -11,64 +11,89 @@ void setup()
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  totale = 0;
+  risDaRaggiungere = 0;
   numeroCorrente = 0;
   turno = 0;
   finito = false;
   cont = 0;
   mossaGiocatore = '0';
+  mossaAppoggio = 0;
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  FaiMossa();
-  delay(1000);
+  ProvaNumeri();
 }
 
-/*void DaiIStruzioni()
+void LetturaMossa() //Metodo che consente la lettura dell'input effettuato dal giocatore
 {
-  Serial.print(istruzioni);
-}*/
-
-void PassaTurno()
-{
-  if (turno == 0)
-  {
-    turno = 1;
-    //istruzioni = "Tocca al Giocatore 2";
-  }
-  else
-  {
-    turno = 0;
-    //istruzioni = "Tocca al Giocatore 1";
-  }
-}
-void AggiungiMossaAlRisultato() //Aggiunge la mossa appena fatta al risultato corrente
-{
-  numeroCorrente = numeroCorrente + mossaGiocatore;
-  Serial.println(numeroCorrente);
+  mossaGiocatore = Serial.readString().toInt();
+  mossaAppoggio = mossaGiocatore;
 }
 
 void FaiMossa() //Permette al giocatore di fare la sua mossa
 {
   while(!finito)
   {
-    mossaGiocatore = Serial.read();
-    if (mossaGiocatore > 0 && mossaGiocatore < 7) 
+    LetturaMossa();
+    if (mossaAppoggio > 0 && mossaAppoggio < 7)
     {
       finito = true;
     }
   }
   finito = false;
-  Serial.println(mossaGiocatore);
-  mossaGiocatore = '0';
-  
+  Serial.println(mossaAppoggio, DEC);  
+  AzzeramentoMosseDelGiocatore();
 }
 
-void TerminaPartita()
+void AggiungiMossaAlRisultato() //Aggiunge la mossa appena fatta al risultato corrente
 {
-  totale = 0;
+  numeroCorrente = numeroCorrente + mossaAppoggio;
+  Serial.println(numeroCorrente);
+}
+
+void AzzeramentoMosseDelGiocatore() //Metodo che azzera la mossa appena fatta dal giocatore
+{
+  mossaGiocatore = '0';
+  mossaAppoggio = 0;
+}
+
+void PassaTurno()
+{
+  if (turno == 0)
+  {
+    turno = 1;
+  }
+  else
+  {
+    turno = 0;
+  }
+}
+
+void DeterminaChiVince() //Metodo che determina chi vince in base a chi ha eseguito l'ultima mossa 
+{
+  if (turno == 0 && numeroCorrente == risDaRaggiungere)
+  {
+    Serial.println("Vince il Giocatore 1");
+  }
+  else if (turno == 1 && numeroCorrente == risDaRaggiungere)
+  {
+    Serial.println("Vince il Giocatore 2");
+  }
+  else if (turno == 0 && numeroCorrente > risDaRaggiungere)
+  {
+    Serial.println("Vince il Giocatore 2");
+  }
+  else if (turno == 1 && numeroCorrente > risDaRaggiungere)
+  {
+  Serial.println("Vince il Giocatore 1"); 
+  }
+}
+
+void TerminaPartita() //Metodo che termina la partita e riazzera le mosse
+{
+  risDaRaggiungere = 0;
   numeroCorrente = 0;
   turno = 0;
 }
