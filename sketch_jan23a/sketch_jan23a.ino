8e99a8e9143de42af77ba6578ecf94b11252789a
Sebastiano Lena <sebastiano.lena@barsanti.edu.it>
2019-01-25 09:47:31 +0100

    Modifiche dei metodi LetturaMossa() e FaiMossa()

--- sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a.ino
@@ -5,52 +5,64 @@ int numeroCorrente; //Indica il numero corrente a cui si è arrivati
 int turno; // turno = 0 (giocatore 1) | turno = 1 (giocatore 2)
 bool finito; //Variabile booleana che verrà utilizzata nei cicli
 int cont; //Contatore che verrà utilizzato nei cicli
+int numTurni; //Variabile che terrà conto del numero dei turni della partita es: Gioc.2 fa la sua mossa e il turno aumenta di 1
+String istruzioni; //Stringa che andrà a visualizzare nel monitor seriale le informazioni necessarie
 
 void setup()
 {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  risDaRaggiungere = 0;
+  risDaRaggiungere = 30;
   numeroCorrente = 0;
   turno = 0;
   finito = false;
   cont = 0;
   mossaGiocatore = '0';
   mossaAppoggio = 0;
+  numTurni = 0;
+  istruzioni = "";
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  ProvaNumeri();
+  FaiMossa();
 }
 
 void LetturaMossa() //Metodo che consente la lettura dell'input effettuato dal giocatore
 {
-  mossaGiocatore = Serial.readString().toInt();
-  mossaAppoggio = mossaGiocatore;
+  while (!finito)
+  {
+    mossaGiocatore = Serial.readString().toInt();
+    mossaAppoggio = mossaGiocatore;
+    if (numTurni == 0 && mossaAppoggio >= 0 && mossaAppoggio < 7)
+    {
+     finito = true;
+    }
+    else if (mossaAppoggio > 0 && mossaAppoggio < 7)
+    {
+     finito = true;
+    }
+  }
+  finito = false;
 }
 
 void FaiMossa() //Permette al giocatore di fare la sua mossa
 {
-  while(!finito)
-  {
-    LetturaMossa();
-    if (mossaAppoggio > 0 && mossaAppoggio < 7)
-    {
-      finito = true;
-    }
-  }
-  finito = false;
-  Serial.println(mossaAppoggio, DEC);  
+  LetturaMossa();
+  Serial.println(mossaAppoggio, DEC);
+  AggiungiMossaAlRisultato();  
   AzzeramentoMosseDelGiocatore();
 }
 
 void AggiungiMossaAlRisultato() //Aggiunge la mossa appena fatta al risultato corrente
 {
   numeroCorrente = numeroCorrente + mossaAppoggio;
+  istruzioni = "Numero Corrente: " + numeroCorrente;
   Serial.println(numeroCorrente);
+  DeterminaChiVince();
+  PassaTurno();
 }
 
 void AzzeramentoMosseDelGiocatore() //Metodo che azzera la mossa appena fatta dal giocatore
@@ -69,6 +81,7 @@ void PassaTurno()
   {
     turno = 0;
   }
+  numTurni++;
 }
 
 void DeterminaChiVince() //Metodo che determina chi vince in base a chi ha eseguito l'ultima mossa 
@@ -96,4 +109,5 @@ void TerminaPartita() //Metodo che termina la partita e riazzera le mosse
   risDaRaggiungere = 0;
   numeroCorrente = 0;
   turno = 0;
+  numTurni = 0;
 }
